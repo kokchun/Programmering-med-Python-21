7c664994b5dfa6ed42f5cb58fd821f0257fea27c
kokchungiang <kokchungiang@gmail.com>
2021-09-07 10:46:36 +0200

    functions code alongs

---
+++ Lectures/Code-alongs/6. functions/geometry.py
@@ -0,0 +1,11 @@
+def triangle_area(base, height): 
+    return base*height/2
+
+def triangle_circumference(side1, side2, side3):
+    return sum(side1, side2, side3)
+
+def square_area(side):
+    return side**2
+
+def square_circumference(side):
+    return side*4
---
+++ Lectures/Code-alongs/6. functions/main.py
@@ -0,0 +1,6 @@
+import geometry as geo
+
+triangle1_area = geo.triangle_area(3,5)
+print(triangle1_area)
+
+print(geo.square_area(10))
