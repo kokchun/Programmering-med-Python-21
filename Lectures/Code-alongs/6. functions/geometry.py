a1108c844eeaed65938cdb7b8c61ea2581e94bc1
kokchungiang <kokchungiang@gmail.com>
2021-09-07 11:01:40 +0200

    fixed a bug

--- Lectures/Code-alongs/6. functions/geometry.py
+++ Lectures/Code-alongs/6. functions/geometry.py
@@ -2,7 +2,7 @@ def triangle_area(base, height):
     return base*height/2
 
 def triangle_circumference(side1, side2, side3):
-    return sum(side1, side2, side3)
+    return sum([side1, side2, side3])
 
 def square_area(side):
     return side**2
